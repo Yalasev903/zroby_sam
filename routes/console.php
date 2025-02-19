a7c5061ffdf1e4d173230dfb95bb4117faf7699f
Yalasev <slasev903@gmail.com>
2025-02-19 08:46:30 +0200

    Create cron whith news

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\FetchNews;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('news:fetch', function () {
+    $this->call(FetchNews::class);
+})->twiceDaily(0, 7); // Запуск в 00:00 и 07:00
